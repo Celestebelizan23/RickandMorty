599ef60053c579c1322cf0cfcdf0dc85231022b4
Celestebelizan23 <celestebelizan23@gmail.com>
2023-03-21 23:23:03 -0300

    finish

--- client/src/components/Favorites.jsx
+++ client/src/components/Favorites.jsx
@@ -1,55 +1,7 @@
-// import { Link } from "react-router-dom";
-// import "../styles/favorites.css";
-// import { useSelector } from "react-redux";
-// import {getFavorites} from "../actions/index";
-// import React from "react";
-// import "../styles/favorites.css";
 
-
-// function Favorites(){
-//   const character = useSelector((state)=> state.favorites)
-//   console.log(character)
-//   return (
-
-//     <div>
-//       <h2 className="title-favorites">favoritos</h2>
-//       <div className='card-deck'>
-//       <div >
-//         <Link to={"/home"}>
-//           <button className="button-back">Go back</button>
-//         </Link>
-//       </div>
-//         {character && character.map((favorite) => (
-//           <div key={favorite.id} className="card-styles">
-//             <img src={favorite.image} className="image-styles" alt={favorite.name} style={{ maxWidth: "300px", maxHeight: "250px" }} />
-
-//             <div className='card-body'>
-//               <h5 className='card-title'>{favorite.name}</h5>
-//               <p className='card-text'>
-//                 <strong>Status:</strong> {favorite.status}
-//               </p>
-//               <p className='card-text'>
-//                 <strong>Species:</strong> {favorite.species}
-//               </p>
-//               <p className='card-text'>
-//               </p>
-//               <p className='card-text'>
-//               </p>
-//             </div>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//    );
-        
-//  };
-
-//  export default Favorites;
- 
 import { Link } from "react-router-dom";
 import "../styles/favorites.css";
 import { useSelector } from "react-redux";
-import {getFavorites} from "../actions/index";
 import React from "react";
 import "../styles/favorites.css";
 
@@ -60,7 +12,7 @@ function Favorites(){
   return (
 
     <div>
-      <h2 className="title-favorites">favoritos</h2>
+      <h2 className="title-favorites">favorites</h2>
       <div className='card-deck'>
       <div >
         <Link to={"/home"}>
